588e5f762d75b0b0020578362e9071157ebf2113
wsyks <syea911@sina.com>
2017-04-14 17:21:57 +0800

    fm

--- syeanfm/src/main.js
+++ syeanfm/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './assets/css/font-awesome.min.css'
+// import 'font-awesome/css/font-awesome.css'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
